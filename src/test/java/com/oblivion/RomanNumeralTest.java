75caa4409d703c8135bed7abfc0f74482d3cf8c6
devopsuser25 <manu.chandrasekhar@accenture.com>
2020-02-20 17:14:02 -0500

    Added 2nd failing test

--- src/test/java/com/oblivion/RomanNumeralTest.java
+++ src/test/java/com/oblivion/RomanNumeralTest.java
@@ -25,6 +25,12 @@ public class RomanNumeralTest {
     void shouldReturn5forV(){
         RomanNumerals romanNumerals = new RomanNumerals();
         assertThat(romanNumerals.getNumeralValue("V"),is(equalTo(5)));
+    }
 
+    @Test
+    @DisplayName("Should Return 5 for V")
+    void shouldReturn10forX(){
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertThat(romanNumerals.getNumeralValue("X"),is(equalTo(10)));
     }
 }
