c1a68b19d6b7a15118b91b29242603b58774b43e
quixoticmonk <manu.chandrasekhar@accenture.com>
2020-02-21 10:53:44 -0500

    Added code to pass the tests for individual numerals

--- src/main/java/com/oblivion/RomanNumerals.java
+++ src/main/java/com/oblivion/RomanNumerals.java
@@ -1,8 +1,30 @@
 package com.oblivion;
 
-public class RomanNumerals {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-    public int getNumeralValue(String v) {
-        return 5;
+public class RomanNumerals {
+    Map<String,Integer> romanNumeralsMap = new HashMap<String, Integer>(){
+        {
+            put("I",1);
+            put("V",5);
+            put("X",10);
+            put("L",50);
+            put("C",100);
+            put("D",500);
+            put("M",1000);
+        }
+    };
+
+    public int getNumeralValue(String inputNumeral) {
+        int returnNumeral =0;
+        for(Map.Entry<String,Integer> entry :romanNumeralsMap.entrySet()){
+            if(inputNumeral.equals(entry.getKey())){
+                returnNumeral = entry.getValue();
+            }
+        }
+        return returnNumeral;
+
     }
 }
--- src/test/java/com/oblivion/RomanNumeralTest.java
+++ src/test/java/com/oblivion/RomanNumeralTest.java
@@ -1,9 +1,7 @@
 package com.oblivion;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
