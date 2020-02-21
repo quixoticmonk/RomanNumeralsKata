f80218fd0111c4a9a66b80ab26bc132d36dca668
quixoticmonk <manu.chandrasekhar@accenture.com>
2020-02-21 10:21:43 -0500

    Added tests for all roman numerals into one test with Junit5 parameterizedtest

--- src/test/java/com/oblivion/RomanNumeralTest.java
+++ src/test/java/com/oblivion/RomanNumeralTest.java
@@ -2,6 +2,9 @@ package com.oblivion;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -20,17 +23,19 @@ public class RomanNumeralTest {
     M	1000*/
 
 
-    @Test
-    @DisplayName("Should Return 5 for V")
-    void shouldReturn5forV(){
+    @DisplayName("Roman Numeral Conversion")
+    @ParameterizedTest(name="Should Return {1} for {0}")
+    @CsvSource({
+            "I,1",
+            "V,5",
+            "X,10",
+            "L,50",
+            "C,100",
+            "D,500",
+            "M,1000"
+    })
+    void shouldReturn10forX(String romanNumeral,int number){
         RomanNumerals romanNumerals = new RomanNumerals();
-        assertThat(romanNumerals.getNumeralValue("V"),is(equalTo(5)));
-    }
-
-    @Test
-    @DisplayName("Should Return 10 for X")
-    void shouldReturn10forX(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertThat(romanNumerals.getNumeralValue("X"),is(equalTo(10)));
+        assertThat(romanNumerals.getNumeralValue(romanNumeral),is(equalTo(number)));
     }
 }
